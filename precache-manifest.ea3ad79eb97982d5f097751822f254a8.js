50a0eb2d79960d9085f45f16bb8dbd37a05a784c
s.kobets <s-kobets617@yandex.ru>
2019-03-05 06:34:25 +0300

    Updates

---
+++ precache-manifest.ea3ad79eb97982d5f097751822f254a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "776741bcb04242ff535e",
+    "url": "/weacher-spa/static/css/main.2b5d5ba8.chunk.css"
+  },
+  {
+    "revision": "776741bcb04242ff535e",
+    "url": "/weacher-spa/static/js/main.776741bc.chunk.js"
+  },
+  {
+    "revision": "0af1aaa8bd3639e70c26",
+    "url": "/weacher-spa/static/js/runtime~main.0af1aaa8.js"
+  },
+  {
+    "revision": "24137c1119bf5f9850d8",
+    "url": "/weacher-spa/static/js/2.24137c11.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weacher-spa/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1315f9ab25e17710a533f973a972ce53",
+    "url": "/weacher-spa/index.html"
+  }
+];
